0e1d8f62317b6f981c51a1b75ed4f173d51037c8
trapple <trapplejp@gmail.com>
2015-10-13 16:43:08 +0900

    stateに変更

--- home/webapp/perl/lib/Isucon5/Web.pm
+++ home/webapp/perl/lib/Isucon5/Web.pm
@@ -138,9 +138,8 @@ sub permitted {
     $another_id == current_user()->{id} || is_friend($another_id);
 }
 
-my $PREFS;
 sub prefectures {
-    $PREFS ||= do {
+    state $PREFS ||= do {
         [
         '未入力',
         '北海道', '青森県', '岩手県', '宮城県', '秋田県', '山形県', '福島県', '茨城県', '栃木県', '群馬県', '埼玉県', '千葉県', '東京都', '神奈川県', '新潟県', '富山県',
